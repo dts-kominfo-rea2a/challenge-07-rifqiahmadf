61e34adc26d8a66c39b541fa4c8f9a429c88d2f9
rifqiahmadf <rahmadfauzi600@gmail.com>
2022-06-10 15:16:43 +0700

    rifqi ahmad fauzi (152235865101-383)

--- promise.js
+++ promise.js
@@ -1,7 +1,22 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (status) => {
+  try {
+    return [
+      ...(await promiseTheaterIXX()),
+      ...(await promiseTheaterVGC()),
+    ].reduce((prev, current) => {
+      if (current?.hasil === status) {
+        return (prev += 1);
+      } else {
+        return prev;
+      }
+    }, 0);
+  } catch (err) {
+    console.log(err.message);
+  }
+};
 
 module.exports = {
   promiseOutput,
